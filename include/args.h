303d48f0651a74643b3f9a26cecd766f9a496131
Teresa Lastname <99054975+ComradeYellowCitrusFruit@users.noreply.github.com>
2022-12-01 18:40:52 -0500

    Changed the license
    Changed the license for the MIT license to the GNU GPLv3, and added
    corresponding copyright info to all of the files.

--- include/args.h
+++ include/args.h
@@ -1,3 +1,12 @@
+/*
+*       This file is part of bfc.
+*
+*       bfv is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+*
+*       bfc is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+*
+*       You should have received a copy of the GNU General Public License along with bfc. If not, see <https://www.gnu.org/licenses/>.
+*/
 #ifndef BRAINFUCK_COMPILER_ARGS_H__
 #define BRAINFUCK_COMPILER_ARGS_H__
 
