dec924b1709d3e7f012c5be68b5fb3f8d2d0a7f5
Teresa Lastname <99054975+ComradeYellowCitrusFruit@users.noreply.github.com>
2022-11-30 23:25:10 -0500

    Fixed an issue where an extra '%' would be printed
    Fixed an issue where an extra '%' would be printed when printing the
    assembly for a syscall.

--- src/syscalls.c
+++ src/syscalls.c
@@ -1,22 +1,22 @@
 #include "include/args.h"
 #include "include/syscalls.h"
 
-char linux_outcall[] = "\tmov $0, %%rax\n\tmov $1, %%rdi\n\tlea arr(, %%ecx), %%rsi\n\tmov $1, %%rdx\n\tpush %%rcx\n\tsyscall\n\tpop %%rcx\n";
-char linux_incall[]  = "\tmov $1, %%rax\n\tmov $1, %%rdi\n\tlea arr(, %%ecx), %%rsi\n\tmov $1, %%rdx\n\tpush %%rcx\n\tsyscall\n\tpop %%rcx\n";
-char linux_exitcall[] = "\tmov $60, %%rax\n\txor %%rdi, %%rdi\n\tsyscall\n";
+char linux_outcall[] = "\tmov $0, %rax\n\tmov $1, %rdi\n\tlea arr(, %ecx), %rsi\n\tmov $1, %rdx\n\tpush %rcx\n\tsyscall\n\tpop %rcx\n";
+char linux_incall[]  = "\tmov $1, %rax\n\tmov $1, %rdi\n\tlea arr(, %ecx), %rsi\n\tmov $1, %rdx\n\tpush %rcx\n\tsyscall\n\tpop %rcx\n";
+char linux_exitcall[] = "\tmov $60, %rax\n\txor %rdi, %rdi\n\tsyscall\n";
 
-char openbsd_outcall[] = "\tmov $4, %%rax\n\tmov $1, %%rdi\n\tlea arr(, %%ecx), %%rsi\n\tmov $2, %%rdx\n\tpush %%rcx\n\tsyscall\n\tpop %%rcx\n";
-char openbsd_incall[] = "\tmov $3, %%rax\n\tmov $1, %%rdi\n\tlea arr(, %%ecx), %%rsi\n\tmov $1, %%rdx\n\tpush %%rcx\n\tsyscall\n\tpop %%rcx\n";
-char openbsd_exitcall[] = "\tmov $1, %%rax\n\txor %%rdi, %%rdi\n\tsyscall\n";
+char openbsd_outcall[] = "\tmov $4, %rax\n\tmov $1, %rdi\n\tlea arr(, %ecx), %rsi\n\tmov $2, %rdx\n\tpush %rcx\n\tsyscall\n\tpop %rcx\n";
+char openbsd_incall[] = "\tmov $3, %rax\n\tmov $1, %rdi\n\tlea arr(, %ecx), %rsi\n\tmov $1, %rdx\n\tpush %rcx\n\tsyscall\n\tpop %rcx\n";
+char openbsd_exitcall[] = "\tmov $1, %rax\n\txor %rdi, %rdi\n\tsyscall\n";
 
-char freebsd_outcall[] = "\tmov $4, %%rax\n\tmov $1, %%rdi\n\tlea arr(, %%ecx), %%rsi\n\tmov $2, %%rdx\n\tpush %%rcx\n\tsyscall\n\tpop %%rcx\n";
-char freebsd_incall[] = "\tmov $3, %%rax\n\tmov $1, %%rdi\n\tlea arr(, %%ecx), %%rsi\n\tmov $1, %%rdx\n\tpush %%rcx\n\tsyscall\n\tpop %%rcx\n";
-char freebsd_exitcall[] = "\tmov $1, %%rax\n\txor %%rdi, %%rdi\n\tsyscall\n";
+char freebsd_outcall[] = "\tmov $4, %rax\n\tmov $1, %rdi\n\tlea arr(, %ecx), %rsi\n\tmov $2, %rdx\n\tpush %rcx\n\tsyscall\n\tpop %rcx\n";
+char freebsd_incall[] = "\tmov $3, %rax\n\tmov $1, %rdi\n\tlea arr(, %ecx), %rsi\n\tmov $1, %rdx\n\tpush %rcx\n\tsyscall\n\tpop %rcx\n";
+char freebsd_exitcall[] = "\tmov $1, %rax\n\txor %rdi, %rdi\n\tsyscall\n";
 
 /* On Windows we can use libc and therefore main because we can't directly use system calls. */
-char win32_outcall[] = "\tpush %%rcx\n\tmovb arr(, %%ecx), %%cl\n\tcall putchar\n\tpop %%rcx\n";
-char win32_incall[] = "\tcall getchar\n\tmovb %%al, arr(, %%ecx)\n";
-char win32_exitcall[] = "\txor %%rax, %%rax\n\tret";
+char win32_outcall[] = "\tpush %rcx\n\tmovb arr(, %ecx), %cl\n\tcall putchar\n\tpop %rcx\n";
+char win32_incall[] = "\tcall getchar\n\tmovb %al, arr(, %ecx)\n";
+char win32_exitcall[] = "\txor %rax, %rax\n\tret";
 
 /* Get the correct write call string */
 char *getOutStr()
